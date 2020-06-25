32ec94f95cfe5241e1d48abdf9c405810aa51ccc
DacKer <66746166+xDacKer@users.noreply.github.com>
2020-06-25 14:59:33 +0200

    Update RSA_Calculator.py

--- RSA_Calculator.py
+++ RSA_Calculator.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+from Crypto.Util.number import long_to_bytes
+
 print(r'''
 __/\\\\\\\\\\\\__________________________________/\\\________/\\\______________________________        
  _\/\\\////////\\\_______________________________\/\\\_____/\\\//_______________________________       
@@ -19,13 +23,17 @@ print('| [3] Totient(N)                       |')
 print('| [4] Plaintext, E & N = Ciphertext    |')
 print('| [5] Q, P & E = D                     |')
 print('| [6] P, Ciphertext, E & N = Plaintext |')
+print('|                                      |')
 print('| [0] Exit the programm                |')
+print('|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|')
+print('|              EXTRA MODE              |')
+print('| [99] Decrypt the picoCTF 2019 flag   |')
 print('¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯')
-
-choice = int(input('Enter the operation number: '))
 
 exit = False
 while exit == False:
+    choice = int(input('Enter the operation number: '))
+
     if choice == 1:
         p = int(input('Enter p: '))
         q = int(input('Enter q: '))
@@ -44,6 +52,34 @@ while exit == False:
     elif choice == 4:
         pt = int(input('Enter plaintext: '))
         e = int(input('Enter e: '))
+        n = int(input('Enter n: '))
+        result = pow(pt, e, n)
+        print("The result is:", result)
+    elif choice == 5:
+        p = int(input('Enter p: '))
+        q = int(input('Enter q: '))
+        e = int(input('Enter e: '))
+        phi = (p - 1) * (q - 1)
+        result = pow(e, -1, phi)
+        print("The result is:", result)
+    elif choice == 6:
+        p = int(input('Enter p: '))
+        ct = int(input('Enter ciphertext: '))
+        e = int(input('Enter e: '))
+        n = int(input('Enter n: '))
+        q = n // p
+        phi = (p - 1) * (q - 1)
+        d = pow(e, -1, phi)
+        result = pow(ct, d, n)
+        print("The result is:", result)
+        picoctf2019 = result
+    elif choice == 99:
+        flag = long_to_bytes(picoctf2019).decode('UTF-8')
+        print(flag)
+    elif choice == 0:
+        exit = True
+    else:
+        print('Wrong number')
         n = int(input('Enter n: '))
         result = pow(pt, e, n)
         print("The result is:", result)
